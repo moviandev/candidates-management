539b0625720e77eae7fc22586424ad11b51f09c7
Matheus Viana <matheus_o_viana@hotmail.com>
2019-11-26 16:41:58 -0300

    fixes: fixed candidatesMModel

--- models/candidatesModel.js
+++ models/candidatesModel.js
@@ -9,45 +9,50 @@ const benefitsSchema = new mongoose.Schema({
   other: { type: String, default: '' }
 });
 
-const candidateSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please provide a least one name'],
-    trim: true
+const candidateSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide a least one name'],
+      trim: true
+    },
+    phone: {
+      type: String,
+      minlength: [14, 'The phone number must have this format (00) 0000-0000'],
+      trim: true
+    },
+    mobile: {
+      type: String,
+      minlength: [
+        15,
+        'The mobile number must have hits format (00) 0.0000-0000'
+      ],
+      trim: true,
+      required: [true, 'Please provide a mobile number']
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide a valid email'],
+      trim: true,
+      validate: [validator.isEmail, 'This email is not valid try again']
+    },
+    tech: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    obs: {
+      type: String,
+      trim: true
+    },
+    lastSalary: {
+      type: Number,
+      required: true
+    },
+    benefits: benefitsSchema
   },
-  phone: {
-    type: String,
-    minlength: [14, 'The phone number must have this format (00) 0000-0000'],
-    trim: true
-  },
-  mobile: {
-    type: String,
-    minlength: [15, 'The mobile number must have hits format (00) 0.0000-0000'],
-    trim: true,
-    required: [true, 'Please provide a mobile number']
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: [true, 'Please provide a valid email'],
-    trim: true,
-    validate: [validator.isEmail, 'This email is not valid try again']
-  },
-  tech: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  obs: {
-    type: String,
-    trim: true
-  },
-  lastSalary: {
-    type: Number,
-    required: true
-  },
-  benefits: benefitsSchema
-});
+  { timestamps: true }
+);
 
 const Candidate = mongoose.model('Candidate', candidateSchema);
 
