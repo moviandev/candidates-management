a639ede22b287892da835fb0e43c928c16eb70a6
Matheus Viana <matheus_o_viana@hotmail.com>
2019-11-27 16:07:13 -0300

    chore: added the empty functions into usersControllers

---
+++ controllers/usersController.js
@@ -0,0 +1,18 @@
+const User = require('../');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const user 
+});
+ 
+exports.getUser = catchAsync(async (req, res, next) => {
+  
+});
+
+exports.createUser = catchAsync(async (req, res, next) => {
+  
+});
+
+exports.updateUser = catchAsync(async (req, res, next) => { });
+
+exports.deleteUser = catchAsync(async (req, res, next) => { });
