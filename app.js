42b4bdcabc50133054946889f24c4cf7439b08ca
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-10 15:26:13 -0300

    chore: added middleware to prevent HTTP parameter polution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErroHandling = require('./controllers/errorsController');
@@ -35,6 +36,9 @@ app.use(xss());
 
 // Setting morgan to DEV to see in our logs the requests Status
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// Preventing http parameters polution
+app.use(hpp());
 
 // Setting body parser
 app.use(express.json({ limit: '10kb' }));
