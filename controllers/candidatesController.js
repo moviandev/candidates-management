3d780ca67283b09218b3305111680554cc2db0f5
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-16 17:34:37 -0300

    refact: deleted unused code

--- controllers/candidatesController.js
+++ controllers/candidatesController.js
@@ -1,7 +1,4 @@
 const Candidate = require('../models/candidatesModel');
-// const catchAsync = require('../utils/catchAsync');
-// const APIFeats = require('../utils/APIFeats');
-// const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
 exports.getAllCandidates = factory.getAll(Candidate);
